782cb75fd9904503b35913a0bd9101c04fad6456
Prathamesh Sonpatki <csonpatki@gmail.com>
2014-08-09 23:40:29 +0530

    Remove hardcoded schema cache

--- lib/schema.rb
+++ lib/schema.rb
@@ -1,12 +1,23 @@
 module Torm
   class SchemaCache
     def initialize connection
+      @connection = connection
+      @columns = {}
+      @columns_hash = {}
     end
+
     def table_exists? name
       true
     end
-    def columns_hash arg
-    { posts: 'author' }
+
+    def columns(table_name)
+      @columns[table_name] ||= @connection.columns table_name
+    end
+
+    def columns_hash(table_name)
+      @columns_hash[table_name] ||= Hash[columns(table_name).map { |col|
+                                           [col.name, col]
+        }]
     end
   end
 end
