bf5c82c39b1ade92ecc298c0c2da376ab7bcedab
Vipul A M <vipulnsward@gmail.com>
2014-08-11 22:28:49 +0530

    - Remove describe

--- test/test_model.rb
+++ test/test_model.rb
@@ -2,10 +2,10 @@ require 'test/helper'
 
 module Torm
   class TestModel < Minitest::Test
-    describe "select query" do
-      def test_accepts_where_clause
-        assert_equal "SELECT FROM posts  WHERE posts.id = 1", Post.where(id: 1).to_sql
-      end
+
+    def test_accepts_where_clause
+      assert_equal "SELECT FROM posts  WHERE posts.id = 1", Post.where(id: 1).to_sql
     end
+
   end
 end
