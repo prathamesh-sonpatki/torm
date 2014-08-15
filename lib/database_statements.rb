3c1d14556ae54d7801042f8b9cf4c23a86c6d14f
Vipul A M <vipulnsward@gmail.com>
2014-08-16 00:19:27 +0530

    Spell out managers

--- lib/database_statements.rb
+++ lib/database_statements.rb
@@ -2,18 +2,18 @@ module Torm
   module DatabaseStatements
     def create attributes
       new_record = new(attributes)
-      cm = Arel::InsertManager.new table.engine
-      cm.into table
-      cm.insert new_record.current_attribute_values(attributes)
-      connection.exec_query cm.to_sql
+      insert_manager = Arel::InsertManager.new table.engine
+      insert_manager.into table
+      insert_manager.insert new_record.current_attribute_values(attributes)
+      connection.exec_query insert_manager.to_sql
       last
     end
 
     def destroy id
-      dm = Arel::DeleteManager.new table.engine
-      dm.from table
-      dm.where table[:id].eq id
-      self.connection.exec_query dm.to_sql
+      delete_manager = Arel::DeleteManager.new table.engine
+      delete_manager.from table
+      delete_manager.where table[:id].eq id
+      self.connection.exec_query delete_manager.to_sql
     end
 
     def find(id)
