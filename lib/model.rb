26c69de9d1e428c96259cc0b21401a54136d1567
Prathamesh Sonpatki <csonpatki@gmail.com>
2014-08-17 22:56:25 +0530

    Write initial set of tests

--- lib/model.rb
+++ lib/model.rb
@@ -16,14 +16,5 @@ module Torm
     def self.table
       @_table ||= Arel::Table.new(self.name.downcase + 's', Torm::Engine.new)
     end
-
-    def self.where(options)
-      k, v   = options.shift
-      clause = table[k].eq v
-      options.each do |k, v|
-        clause = clause.and(table[k].eq(v))
-      end
-      table.where(clause)
-    end
   end
 end
--- test/test_model.rb
+++ test/test_model.rb
@@ -2,10 +2,30 @@ require 'test/helper'
 
 module Torm
   class TestModel < Minitest::Test
-    describe "select query" do
-      def test_accepts_where_clause
-        assert_equal "SELECT FROM posts  WHERE posts.id = 1", ::Post.where(id: 1).to_sql
-      end
+    def test_accepts_where_clause
+      assert_equal "SELECT FROM posts  WHERE posts.id = 1", Post.where(id: 1).to_sql
+    end
+
+    def test_creates_appropriate_arel_table
+      table = Post.table
+      assert_instance_of Arel::Table, table
+      assert_equal  'posts', table.name
+    end
+
+    def test_initialize_allows_setting_attributes
+      post = Post.new subject: "Lol Nom Rom"
+      assert_equal "Lol Nom Rom", post.name
+    end
+
+    def test_save_creates_new_record
+      Post.delete_all
+      post = Post.new subject: "Lol Nom Rom"
+      post.save
+      assert_equal 1, Post.count
+    end
+
+    def test_delete_all_creates_sql
+      assert_equal "DELETE FROM posts", Post.delete_all
     end
   end
 end
