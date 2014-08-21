b4a96977efcc15e76d376e2980db57f682e97426
Prathamesh Sonpatki <csonpatki@gmail.com>
2014-08-21 06:36:17 -0500

    Added sample result of columns data from table

--- lib/adapters/postgres_adapter.rb
+++ lib/adapters/postgres_adapter.rb
@@ -27,14 +27,15 @@ module Torm
         end
       end
 
+      # [["id", "integer", nil, "t", "23", "-1"], ["title", "text", nil, "f", "25", "-1"]]
       def column_definitions(name)
         @connection.exec(%Q(SELECT a.attname, format_type(a.atttypid, a.atttypmod),
-                     pg_get_expr(d.adbin, d.adrelid), a.attnotnull, a.atttypid, a.atttypmod
-                FROM pg_attribute a LEFT JOIN pg_attrdef d
-                  ON a.attrelid = d.adrelid AND a.attnum = d.adnum
-                  WHERE a.attrelid = '#{name}'::regclass
-                 AND a.attnum > 0 AND NOT a.attisdropped
-               ORDER BY a.attnum))
+                            pg_get_expr(d.adbin, d.adrelid), a.attnotnull, a.atttypid, a.atttypmod
+                            FROM pg_attribute a LEFT JOIN pg_attrdef d
+                            ON a.attrelid = d.adrelid AND a.attnum = d.adnum
+                            WHERE a.attrelid = '#{name}'::regclass
+                            AND a.attnum > 0 AND NOT a.attisdropped
+                            ORDER BY a.attnum))
       end
     end
   end
--- lib/column.rb
+++ lib/column.rb
@@ -5,10 +5,9 @@ module Torm
   class Column
     attr_accessor :name, :sql_type, :cast_type
 
-    def initialize(name, sql_type, cast_type)
+    def initialize(name, sql_type)
       @name = name
       @sql_type = sql_type
-      @cast_type = cast_type
     end
   end
 end
