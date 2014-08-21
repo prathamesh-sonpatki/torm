1d96ca66491e319e228e6673a412a73b0a8b6b13
Prathamesh Sonpatki <csonpatki@gmail.com>
2014-08-21 06:26:24 -0500

    Removed column specific things

--- lib/adapters/postgres_adapter.rb
+++ lib/adapters/postgres_adapter.rb
@@ -20,22 +20,6 @@ module Torm
       def quote value, column = nil
         "'#{value.to_s}'"
       end
-
-      def columns
-        column_definitions('posts').values.map do |column|
-          Torm::Column.new(column[0], column[1])
-        end
-      end
-
-      def column_definitions(name)
-        @connection.exec(%Q(SELECT a.attname, format_type(a.atttypid, a.atttypmod),
-                     pg_get_expr(d.adbin, d.adrelid), a.attnotnull, a.atttypid, a.atttypmod
-                FROM pg_attribute a LEFT JOIN pg_attrdef d
-                  ON a.attrelid = d.adrelid AND a.attnum = d.adnum
-                  WHERE a.attrelid = '#{name}'::regclass
-                 AND a.attnum > 0 AND NOT a.attisdropped
-               ORDER BY a.attnum))
-      end
     end
   end
 end
